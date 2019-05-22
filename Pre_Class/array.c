be7fa3c9e8d81c4aad7d5ab672c1f64329dc7ba4
marey111 <marey111@126.com>
2019-05-22 20:02:52 +1000

    References

---
+++ Pre_Class/array.c
@@ -0,0 +1,59 @@
+//
+// Created by Rui.Mu on 2019-05-22.
+//
+
+// https://www.cnblogs.com/onepixel/p/7674659.html
+
+#include <stdio.h> // include standard I/O library defs and functions
+
+#define SIZE 6     // define a symbolic constant
+
+/**
+ * bubble sorting
+ * @param array
+ * @param n
+ */
+void bubbleSort(int array[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {        // 相邻元素两两对比
+                int temp = array[j + 1];        // 元素交换
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+/**
+ * insertion sorting
+ * @param array
+ * @param n
+ */
+void insertionSort(int array[], int n) {  // function headers must provide types
+    int i;                                 // each variable must have a type
+    for (i = 1; i < n; i++) {              // for-loop syntax
+        int element = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > element) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = element;
+    }
+}
+
+// logical AND
+// abbreviated assignment j=j-1
+// statements terminated by ;
+// code blocks enclosed in { }
+int main(void) {
+    int numbers[SIZE] = {3, 6, 5, 2, 4, 1};  /* array declaration
+                                                 and initialisation */
+    int i;
+    insertionSort(numbers, SIZE);
+    for (i = 0; i < SIZE; i++)
+        printf("%d\n", numbers[i]);             // printf defined in <stdio>
+    return 0;
+}
+
---
+++ Pre_Class/stack.h
@@ -0,0 +1,15 @@
+//
+// Created by Rui.Mu on 2019-05-22.
+//
+
+#ifndef COMP9024_19T2_STACK_H
+#define COMP9024_19T2_STACK_H
+
+#endif //COMP9024_19T2_STACK_H
+
+#define MAXITEMS 10
+
+void StackInit();      // set up empty stack
+int  StackIsEmpty();   // check whether stack is empty
+void StackPush(char);  // insert char on top of stack
+char StackPop();       // remove char from top of stack
